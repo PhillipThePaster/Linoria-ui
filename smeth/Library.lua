ca74df7d02103b7fcb5a8be19df272d8fbca9342
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-08-22 00:48:03 -0400

    Update Library.lua

--- smeth/Library.lua
+++ smeth/Library.lua
@@ -2797,7 +2797,7 @@ do
 
     local KeybindLabel = Library:CreateLabel({
         Size = UDim2.new(1, 0, 0, 20);
-        Position = UDim2.fromOffset(5, 20),
+        Position = UDim2.fromOffset(5, 2),
         TextXAlignment = Enum.TextXAlignment.Left,
 
         Text = 'Keybinds';
