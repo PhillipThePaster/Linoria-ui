4a86efc1a1494788f45545203788ad6deebf82a2
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-09-17 12:34:11 -0400

    Update Library.lua

--- smeth/Library.lua
+++ smeth/Library.lua
@@ -3026,9 +3026,9 @@ function Library:CreateWindow(...)
 
     local TabArea = Library:Create('Frame', {
         BackgroundTransparency = 1;
-        Position = UDim2.new(0, 8, 400, 8);
+        Position = UDim2.new(0, 8, 0, 8);
         Size = UDim2.new(1, -16, 0, 21);
-        ZIndex = 3;
+        ZIndex = 1;
         Parent = MainSectionInner;
     });
 
@@ -3042,7 +3042,7 @@ function Library:CreateWindow(...)
     local TabContainer = Library:Create('Frame', {
         BackgroundColor3 = Library.MainColor;
         BorderColor3 = Library.OutlineColor;
-        Position = UDim2.new(0, 8, 0, 5);
+        Position = UDim2.new(0, 8, 0, 30);
         Size = UDim2.new(1, -16, 1, -38);
         ZIndex = 2;
         Parent = MainSectionInner;
